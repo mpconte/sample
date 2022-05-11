8a659b28711aad1b65b34945c2a818eb4ba07883
mconte <Matthew.Conte@spx.com>
2022-05-11 11:11:59 -0400

    Remove boost shared linkages in CMakeLists.txt. Add boost python code for testing purposes

--- code/main.cpp
+++ code/main.cpp
@@ -1,12 +1,13 @@
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
+#include <boost/python/dict.hpp>
 #include <iostream>
 
 using namespace boost::filesystem;
 
 int main(int argc, char *argv[]) {
     path p(argc>1? argv[1] : ".");
-
+    boost::python::dict sample_dict;
     if(is_directory(p)) {
         std::cout << p << " is a directory containing:\n";
 
