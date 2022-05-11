e6b1090f1d5e5ddf4bec43f833f79afa456180fe
mconte <Matthew.Conte@spx.com>
2022-05-11 14:18:18 -0400

    Reproduce issue with boost::python::numpy::get_data call

--- code/main.cpp
+++ code/main.cpp
@@ -1,13 +1,27 @@
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
-#include <boost/python/dict.hpp>
+#include <boost/python/numpy.hpp>
 #include <iostream>
 
 using namespace boost::filesystem;
 
 int main(int argc, char *argv[]) {
-    path p(argc>1? argv[1] : ".");
-    boost::python::dict sample_dict;
+    path p(argc>1? argv[1] : ".");      
+    double array[2][2] = { { 1, 2,}, { 3, 4 }};    
+
+    int type_size = sizeof(int);    
+    boost::python::tuple shape = boost::python::make_tuple<int, int>(2, 2);
+    boost::python::tuple strides = boost::python::make_tuple(type_size * 2, type_size * 1); 
+        
+    boost::python::numpy::ndarray np_array = boost::python::numpy::from_data(
+        array, // array of data
+        boost::python::numpy::dtype::get_builtin<double>(), // type
+        shape, // shape
+        strides, // strides
+        boost::python::object() // owner object
+    );
+    auto data = np_array.get_data();
+    
     if(is_directory(p)) {
         std::cout << p << " is a directory containing:\n";
 
